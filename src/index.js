9f6861e68f8e3fc3e908e9954e4581697d31bc51
rlane85 <ryanlane1985@gmail.com>
2020-07-19 18:26:13 -0400

    rename and create story

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Demo from "./demo";
+import Demo from "./Autocomplete";
 
 ReactDOM.render(<Demo />, document.querySelector("#root"));
