dc3f3a2d92e98fa37be1c55b6b0399b5de271784
rlane85 <ryanlane1985@gmail.com>
2020-07-19 21:15:22 -0400

    props for codesandbox.io

--- src/index.js
+++ src/index.js
@@ -2,4 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Demo from "./Autocomplete";
 
-ReactDOM.render(<Demo />, document.querySelector("#root"));
+const countriesUrl =
+  "https://country.register.gov.uk/records.json?page-size=5000";
+const mdiTagUrl = "https://materialdesignicons.com/api/tags";
+const mdiUrl =
+  "https://cors-anywhere.herokuapp.com/https://materialdesignicons.com/api/package/38EF63D0-4744-11E4-B3CF-842B2B6CFE1B";
+const sandBoxProps = {
+	type: "icons",
+	tagUrl: mdiTagUrl,
+	url: mdiUrl,
+}
+ReactDOM.render(<Demo {{...sandBoxProps}} />, document.querySelector("#root"));
