6ff077893cc3841523e1fe2a8ea2425cb0f2e0b4
rlane85 <ryanlane1985@gmail.com>
2020-07-19 20:57:14 -0400

    addings icons

--- src/stories/2-Autocomplete.stories.js
+++ src/stories/2-Autocomplete.stories.js
@@ -6,5 +6,16 @@ export default {
   title: "FreeSoloCreateOption",
   component: FreeSoloCreateOption,
 };
+const countriesUrl =
+  "https://country.register.gov.uk/records.json?page-size=5000";
+const mdiTagUrl = "https://materialdesignicons.com/api/tags";
+const mdiUrl =
+  "https://cors-anywhere.herokuapp.com/https://materialdesignicons.com/api/package/38EF63D0-4744-11E4-B3CF-842B2B6CFE1B";
 
-export const Async = () => <FreeSoloCreateOption onClick={action("clicked")} />;
+export const Async = () => (
+  <FreeSoloCreateOption type="countries" url={countriesUrl} onClick={action("clicked")} />
+);
+
+export const AsyncWithIcons = () => (
+  <FreeSoloCreateOption type="icons" tagUrl={mdiTagUrl} url={mdiUrl} onClick={action("clicked")} />
+);
