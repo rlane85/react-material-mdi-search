b36f7f04367e1e1b80f38414eba3e4fd3e9b7b51
rlane85 <ryanlane1985@gmail.com>
2020-07-20 06:30:08 -0400

    started hosting mdi icon paths

--- src/stories/2-Autocomplete.stories.js
+++ src/stories/2-Autocomplete.stories.js
@@ -8,9 +8,9 @@ export default {
 };
 const countriesUrl =
   "https://country.register.gov.uk/records.json?page-size=5000";
-const mdiTagUrl = "https://materialdesignicons.com/api/tags";
+const mdiTagUrl = "http://panelpi.ddns.net:3002/iconTags";
 const mdiUrl =
-  "https://cors-anywhere.herokuapp.com/https://materialdesignicons.com/api/package/38EF63D0-4744-11E4-B3CF-842B2B6CFE1B?tag=user";
+  "http://panelpi.ddns.net:3002/icons";
   //forcing tag to minimize download size for now
 export const Async = () => (
   <FreeSoloCreateOption type="countries" url={countriesUrl} onClick={action("clicked")} />
