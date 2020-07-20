e4418cf9afd9ea77292dcf2438b45964d8dbb609
rlane85 <ryanlane1985@gmail.com>
2020-07-20 08:13:14 -0400

    update link

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const countriesUrl =
   "https://country.register.gov.uk/records.json?page-size=5000";
 const mdiTagUrl = "https://materialdesignicons.com/api/tags";
 const mdiUrl =
-  "https://cors-anywhere.herokuapp.com/https://materialdesignicons.com/api/package/38EF63D0-4744-11E4-B3CF-842B2B6CFE1B";
+  "http://panelpi.ddns.net:3002/icons";
 const sandBoxProps = {
   type: "icons",
   tagUrl: mdiTagUrl,
